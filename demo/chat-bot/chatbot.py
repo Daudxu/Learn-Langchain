559812cca61fbeacf62f0f2b2b32bd6435b2b8c5
Dane <875126243@qq.com>
2025-04-24 22:33:58 +0800

    fix  update

--- demo/chat-bot/chatbot.py
+++ demo/chat-bot/chatbot.py
@@ -10,6 +10,7 @@ base_url = os.getenv("BASE_URL")
 model_api_key = os.getenv("MODEL_API_KEY")
 model_name = os.getenv("MODEL_NAME")
 
+
 def main():
     llm = ChatOpenAI(
         base_url=base_url,
